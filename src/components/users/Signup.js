d726355045ac1842c008f9d70cbb80b2bbc8df3c
Ranjan Kumar <ranjanjha111@gmail.com>
2021-02-17 23:55:37 +0530

    Signup page changes

--- src/components/users/Signup.js
+++ src/components/users/Signup.js
@@ -8,21 +8,47 @@ const Signup = () => {
         name: '',
         email: '',
         password: '',
+        confirm_password: '',
         company: '',
         country: '',
         error: '',
+        isDisabled: true,
         success: false
     });
 
-    const { name, email, password, company, country, success, error } = values;
+    const { name, email, password, confirm_password, company, country, success, isDisabled, error } = values;
 
     const handleChange = name => event => {
         setValues({ ...values, error: false, [name]: event.target.value });
     };
 
+    const verifySingupButton = (isChecked = false) => {
+        if(name && email && password && company && country && isChecked) {
+            setValues({...values, isDisabled: false})
+        }else {
+            setValues({...values, isDisabled: true})
+        }
+    }
+
+    const handleBlur = (event) => {
+        if(password !== confirm_password) {
+            setValues({ ...values, error: 'Confirm password not match with password.', success: false });
+        }
+
+        verifySingupButton()
+    };
+
+    const handleCheckBox = (event) => {
+        if(event.target.checked) {
+            verifySingupButton(true)
+        } else {
+            verifySingupButton(false)
+        }
+    }
+
     const clickSubmit = event => {
         event.preventDefault();
-        setValues({ ...values, error: false });
+
         signup({ name, email, password, company, country }).then(data => {
             if (data.error) {
                 setValues({ ...values, error: data.error, success: false });
@@ -45,30 +71,52 @@ const Signup = () => {
         <form>
             <div className="form-group">
                 <label className="text-muted">Name</label>
-                <input onChange={handleChange('name')} type="text" className="form-control" value={name} />
+                <input onBlur={handleBlur} onChange={handleChange('name')} type="text" className="form-control" value={name} />
             </div>
 
             <div className="form-group">
                 <label className="text-muted">Company</label>
-                <input onChange={handleChange('company')} type="text" className="form-control" value={company} />
+                <input onBlur={handleBlur} onChange={handleChange('company')} type="text" className="form-control" value={company} />
             </div>
+
+            {/* <div className="form-group">
+                <label className="text-muted">Country</label>
+                <input onBlur={handleBlur} onChange={handleChange('country')} type="text" className="form-control" value={country} />
+            </div> */}
 
             <div className="form-group">
                 <label className="text-muted">Country</label>
-                <input onChange={handleChange('country')} type="text" className="form-control" value={country} />
+                <select className="form-control" onChange={handleChange('country')}>
+                    <option value="0">Select Country</option>
+                    <option value="USA">U.S.A.</option>
+                    <option value="UK">U.K.</option>
+                    <option value="India">India</option>
+                    <option value="Canada">Canada</option>
+                </select>
+                {/* <input onBlur={handleBlur} onChange={handleChange('country')} type="text" className="form-control" value={country} /> */}
             </div>
 
             <div className="form-group">
                 <label className="text-muted">Email</label>
-                <input onChange={handleChange('email')} type="email" className="form-control" value={email} />
+                <input onBlur={handleBlur} onChange={handleChange('email')} type="email" className="form-control" value={email} />
             </div>
 
             <div className="form-group">
                 <label className="text-muted">Password</label>
                 <input onChange={handleChange('password')} type="password" className="form-control" value={password} />
             </div>
-            <button onClick={clickSubmit} className="btn btn-primary">
-                Submit
+            <div className="form-group">
+                <label className="text-muted">Confirm Password</label>
+                <input onBlur={handleBlur} onChange={handleChange('confirm_password')} type="password" className="form-control" value={confirm_password} />
+            </div>
+            <div className="form-group">
+                {/* <input onChange={() => handleCheckBox(!term_and_condition)} type="checkbox" value={term_and_condition} /> */}
+                <input type="checkbox" onChange={handleCheckBox} />
+
+                <label className="text-muted">Term & condition</label>
+            </div>
+            <button onClick={clickSubmit} disabled={isDisabled} className="btn btn-primary">
+                Sign Up
             </button>
         </form>
     );
