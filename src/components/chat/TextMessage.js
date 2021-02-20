cd0ab3daa2bfa731fc085532553a4f3c0e2a5f58
Ranjan Kumar <ranjanjha111@gmail.com>
2021-02-20 10:35:18 +0530

    make url as link in chat

--- src/components/chat/TextMessage.js
+++ src/components/chat/TextMessage.js
@@ -1,11 +1,15 @@
 import React from "react";
 import moment from 'moment';
+import Linkify from 'react-linkify';
 
 const TextMessage = ({ message, messageClass }) => {
     return (
         <div className={`message-row ${messageClass}`}>
             <div className="message-content">
-                <div className="message-text">{ message.message }</div>
+                <div className="message-text">
+                    <Linkify>{message.message}</Linkify>
+                </div>
+                {/* <div className="message-text">{ message.message }</div> */}
                 <div className="message-time">{ moment(message.createdAt).format('LT') }</div>
             </div>
         </div>
